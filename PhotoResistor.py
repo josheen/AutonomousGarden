74ac8954ccecd8b113042ae69d212e2a75f98bc3
josheenyen99 <josheen@ualberta.ca>
2019-12-03 17:08:05 -0700

    added temp error handling

--- PhotoResistor.py
+++ PhotoResistor.py
@@ -2,22 +2,31 @@ import numpy
 import matplotlib.pyplot as plt 
 from drawnow import * 
 import serial
+import time
 
 lightValue = []
 arduinoAnalog = serial.Serial('/dev/cu.usbmodem143201 (Arduino/Genuino Mega or Mega3560)',9600)
 plt.ion()
 
 def makeFigure():
+    plt.ylim(50,850)
     plt.plot(lightValue, 'ro-')
+    plt.title('Photoresistor Reading')
+    plt.ylabel('lumens')
+    plt.xlabel('time')
 
 
 while True:
     while(arduinoAnalog.inWaiting()==0):
         pass
+    arduinoAnalog.inputFlush()
+    time.sleep(0.01) 
     arduinoString = arduinoData.readline()
+
     try:
-        conversion = float(arduinoString)
+        conversion = int(arduinoString.decode('utf-8')) #data is stored at unicode transformation format 
     except:
         pass 
     lightValue.append(conversion)
     plt.pause(0.00001)
+
